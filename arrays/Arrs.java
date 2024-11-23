ad651277e3f7a24a031d88fbd35f5a3ab5904c57
Nishan Bista <nishanbista525@gmail.com>
2024-11-23 14:43:24 +0545

    arr deletion

--- arrays/Arrs.java
+++ arrays/Arrs.java
@@ -24,7 +24,7 @@ public class Arrs {
             } else {
 
                 newArr[i] = newArr[i - 1];
-                
+
             }
 
         }
@@ -33,13 +33,40 @@ public class Arrs {
 
     }
 
+    static int[] deletion(int[] arr, int position) {
+
+        int elementValue = arr[position];
+
+        // shifting elements
+        for (int i = position; i < arr.length - 1; i++) {
+            arr[i] = arr[i + 1];
+        }
+
+        // creating new array with -1 length
+        int[] result = new int[arr.length - 1];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = arr[i];
+        }
+        
+
+        return result;
+    }
+
     public static void main(String[] args) {
 
         int[] myArr = { 1, 2, 3, 4, 5, 6, 7, 8 };
 
         int[] after = insertion(myArr, 0, 100);
 
-        System.err.println("Before: " + Arrays.toString(myArr) + " After: " + Arrays.toString(after));
+        System.out.println("Before: " + Arrays.toString(myArr) + " After: " + Arrays.toString(after));
+
+        int[] anotherArr = { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+        int[] afterDeletion = deletion(anotherArr, 4);
+
+        System.out.println(
+                "Before deletion: " + Arrays.toString(anotherArr) + " After deletion: "
+                        + Arrays.toString(afterDeletion));
 
     }
 }
