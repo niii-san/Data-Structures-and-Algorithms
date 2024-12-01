9a22ac399b711e01d47e69b1a03904f2a409d4d1
Nishan Bista <nishanbista525@gmail.com>
2024-12-01 20:35:17 +0545

    singly linked list

--- Playground.java
+++ Playground.java
@@ -1,16 +1,26 @@
-import java.util.Arrays;
-
-import leetcode.RemoveElement;
+import linkedlist.SinglyLinkedList;
+import linkedlist.SinglyLinkedList.Node;
 
 public class Playground {
     public static void main(String[] args) {
 
-        int[] nums = { 3, 2, 2, 3 };
-        System.out.println("before "+Arrays.toString(nums));
-        int val = 3;
-        RemoveElement removeElement = new RemoveElement();
-        int[] result = removeElement.removeElement(nums, val);
-        System.err.println("after "+Arrays.toString(result));
+        SinglyLinkedList l1 = new SinglyLinkedList();
+
+        Node n1 = l1.addNode(1);
+        Node n2 = l1.addNode(22);
+        Node n3 = l1.addNode(333);
+        Node n4 = l1.addNode(4444);
+        Node n5 = l1.addNode(55555);
+
+        System.out.println("\nbefore");
+        l1.printList();
+
+
+        l1.addAfter(n5,9999999);
+        
+
+        System.out.println("\nafter");
+        l1.printList();
 
     }
 }
--- linkedlist/SinglyLinkedList.java
+++ linkedlist/SinglyLinkedList.java
@@ -5,9 +5,9 @@ public class SinglyLinkedList {
     Node head = null;
     Node tail = null;
 
-    static class Node {
-        int data;
-        Node next;
+    public static class Node {
+        public int data;
+        public Node next;
 
         Node(int data) {
             this.data = data;
@@ -15,7 +15,8 @@ public class SinglyLinkedList {
         }
     }
 
-    public void addNode(int data) {
+    // * PASSED
+    public Node addNode(int data) {
 
         Node newNode = new Node(data);
         if (head == null) {
@@ -25,17 +26,10 @@ public class SinglyLinkedList {
             tail.next = newNode;
             tail = newNode;
         }
+        return newNode;
     }
 
-    public void printList() {
-        Node currentNode = head;
-        while (currentNode != null) {
-            System.out.println(currentNode.data);
-            currentNode = currentNode.next;
-
-        }
-    }
-
+    // * PASSED
     public void addHeadFirst(int data) {
         Node newNode = new Node(data);
         if (head == null) {
@@ -43,6 +37,106 @@ public class SinglyLinkedList {
         } else {
             newNode.next = head;
             head = newNode;
+
+        }
+    }
+
+    // * PASSED
+    public Node addBefore(Node node, int data) {
+
+        Node newNode = new Node(data);
+
+        if (node == head) {
+
+            newNode.next = head;
+            head = newNode;
+            return newNode;
+
+        } else {
+
+            Node current = head;
+            while (current.next != node) {
+                current = current.next;
+            }
+            newNode.next = current.next;
+            current.next = newNode;
+
+            return newNode;
+        }
+    }
+
+    // * PASSED
+    public Node addAfter(Node node, int data) {
+
+        Node newNode = new Node(data);
+        newNode.next = node.next;
+        node.next = newNode;
+
+        return newNode;
+
+    }
+
+    // * PASSED
+    public void removeAfter(Node node) {
+
+        Node temp = node.next;
+        node.next = node.next.next;
+        temp.next = null;
+
+    }
+
+    // * PASSED
+    public void removeBefore(Node node) {
+
+        Node current = head;
+
+        if (node == head) {
+            System.out.println("theres no any node before head");
+        } else {
+            while (current.next.next != node) {
+                if (current.next.next == null) {
+                    System.out.println("Node not found or too few nodes only");
+                    break;
+                }
+                current = current.next;
+            }
+
+            Node temp = current.next;
+            current.next = current.next.next;
+            temp.next = null;
+
+        }
+
+    }
+
+    // * PASSED
+    public void removeNodeByPosition(int position) {
+        // for head
+        if (position == 1) {
+            Node temp = head;
+            head = head.next;
+            temp.next = null;
+        } else {
+            Node current = head;
+            for (int i = 2; i < position; i++) {
+                if (current == null) {
+                    System.out.println("position out of bound");
+                    break;
+                }
+                current = current.next;
+            }
+            Node temp = current.next;
+            current.next = temp.next;
+            temp.next = null;
+        }
+    }
+
+    // * PASSED
+    public void printList() {
+        Node currentNode = head;
+        while (currentNode != null) {
+            System.out.println(currentNode.data);
+            currentNode = currentNode.next;
 
         }
     }
